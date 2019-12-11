6dc9e97ecb6f797e503d6ffbfece11d9e53bfa09
callum-mccracken <callum189@gmail.com>
2019-12-11 14:51:16 -0800

    cross_sections work

---
+++ cross_sections/plot_cross_section.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+sigma_gamma_integ = "/global/scratch/ccmccracken/npsm/cross_sections/nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_1m/sigma_gamma_integ_nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_1m_NCSMC_E1M1E2_Li9_2J_1.agr"
+fig_name = "cross_section_1m.png"
+
+with open(sigma_gamma_integ, "r+") as sig_file:
+    lines = sig_file.readlines()
+
+# ignore the first and last lines
+lines = lines[1:-1]
+
+energies = []
+cross_sections = []
+for line in lines:
+    words = line.split()
+    e = float(words[0])
+    energies.append(e)
+    c = float(words[2]) * 1000
+    cross_sections.append(c)
+
+
+plt.plot(energies, cross_sections)
+plt.title("Cross-Section for The $^8Li(n, \\gamma) ^9Li$ Reaction, $6\\hbar \\Omega$")
+plt.xlabel("Energy ($MeV$)")
+plt.ylabel("Cross-Section ($mb$)")
+plt.savefig(fig_name)
