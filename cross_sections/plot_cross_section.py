b99216a1c8ba96048c3968fe146369b924f4cbf6
callum-mccracken <callum189@gmail.com>
2019-12-11 15:05:52 -0800

    a

--- cross_sections/plot_cross_section.py
+++ cross_sections/plot_cross_section.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-sigma_gamma_integ = "/global/scratch/ccmccracken/npsm/cross_sections/nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_1m/sigma_gamma_integ_nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_1m_NCSMC_E1M1E2_Li9_2J_1.agr"
-fig_name = "cross_section_1m.png"
+sigma_gamma_integ = "/global/scratch/ccmccracken/npsm/cross_sections/nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_3m/sigma_gamma_integ_nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_3m_NCSMC_E1M1E2_Li9_2J_3.agr"
+fig_name = "cross_section_3m.png"
 
 with open(sigma_gamma_integ, "r+") as sig_file:
     lines = sig_file.readlines()
@@ -15,12 +15,12 @@ for line in lines:
     words = line.split()
     e = float(words[0])
     energies.append(e)
-    c = float(words[2]) * 1000
+    c = float(words[2]) * 10000
     cross_sections.append(c)
 
 
 plt.plot(energies, cross_sections)
 plt.title("Cross-Section for The $^8Li(n, \\gamma) ^9Li$ Reaction, $6\\hbar \\Omega$")
 plt.xlabel("Energy ($MeV$)")
-plt.ylabel("Cross-Section ($mb$)")
+plt.ylabel("Cross-Section ($\\mu b$)")
 plt.savefig(fig_name)
