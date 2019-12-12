680d25ff3119b9d4533e34c926b5bc437bcc585d
callum-mccracken <callum189@gmail.com>
2019-12-12 09:19:05 -0800

    a

--- cross_sections/plot_cross_section.py
+++ cross_sections/plot_cross_section.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-sigma_gamma_integ = "/global/scratch/ccmccracken/npsm/cross_sections/nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_3m/sigma_gamma_integ_nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_3m_NCSMC_E1M1E2_Li9_2J_3.agr"
-fig_name = "cross_section_3m.png"
+sigma_gamma_integ = "/global/scratch/ccmccracken/npsm/cross_sections/nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_1m/sigma_gamma_integ_nLi8_n3lo-NN3Nlnl-srg2.0_20_Nmax6_pheno_all_adjusted_1m_NCSMC_E1M1E2_Li9_2J_1.agr"
+fig_name = "cross_section_1m.png"
 
 with open(sigma_gamma_integ, "r+") as sig_file:
     lines = sig_file.readlines()
@@ -23,4 +23,7 @@ plt.plot(energies, cross_sections)
 plt.title("Cross-Section for The $^8Li(n, \\gamma) ^9Li$ Reaction, $6\\hbar \\Omega$")
 plt.xlabel("Energy ($MeV$)")
 plt.ylabel("Cross-Section ($\\mu b$)")
+plt.yscale("log")
+plt.xlim(0,1.1)
+plt.xticks(ticks=[0.0,0.2,0.4,0.6,0.8,1.0])
 plt.savefig(fig_name)
